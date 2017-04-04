dd5b8d0653ff4e710fe35175b544c0c178d5148c
Avarea <valchek@abv.bg>
2017-04-04 19:19:07 +0300

    Finishing Homework solutions for Data Types and Variables - Lab

---
+++ IntegerAndRealNumbers/9RefactorSpecialNumbers/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+
+namespace _9RefactorSpecialNumbers
+{
+    class Program
+    {
+        static void Main()
+        {
+            int maxNumber = int.Parse(Console.ReadLine());
+            int sum = 0;
+            int currentNumber = 0;
+            for (int i = 1; i <= maxNumber; i++)
+            {
+                currentNumber = i;
+                while (i > 0)
+                {
+                    sum += i % 10;
+                    i = i / 10;
+                }
+                bool isItSpecial = (sum == 5) || (sum == 7) || (sum == 11);
+                Console.WriteLine($"{currentNumber} -> {isItSpecial}");
+                sum = 0;
+                i = currentNumber;
+            }
+        }
+    }
+}
