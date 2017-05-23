31a860cc0a6fa57fe1c7602d9eaba36f4d5ded30
Avarea <valchek@abv.bg>
2017-05-23 12:36:23 +0300

    C# Syntax exercises

---
+++ IntroAndSyntax/04BeverageLabels/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+
+namespace _04BeverageLabels
+{
+    class Program
+    {
+        static void Main()
+        {
+            string beverage = Console.ReadLine();
+            int volume = int.Parse(Console.ReadLine());
+            int energyContent = int.Parse(Console.ReadLine());
+            int sugarContent = int.Parse(Console.ReadLine());
+
+            double energy = (volume * energyContent)/100;
+            double sugar = (volume * (double)sugarContent)/100;
+
+            Console.WriteLine($"{volume}ml {beverage}:");
+            Console.WriteLine($"{energy}kcal, {sugar}g sugars");
+        }
+    }
+}
