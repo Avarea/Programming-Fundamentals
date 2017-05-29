562a9514d7768cd64fe2693aeb8b9fc2799d5879
Avarea <valchek@abv.bg>
2017-05-29 22:03:23 +0300

    Conditional Statements and Loops - Problem 10. Multiplication Table 2.0

---
+++ ConditionalStatementsAndLoops/10MultiplicationTable2.0/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace _10MultiplicationTable2._0
+{
+    class Program
+    {
+        static void Main()
+        {
+            int multiplier = int.Parse(Console.ReadLine());
+            int start = int.Parse(Console.ReadLine());
+            if (start > 10)
+            {
+                Console.WriteLine($"{multiplier} X {start} = {multiplier * start}");
+            }
+            else
+            {
+                for (int i = start; i <= 10; i++)
+                {
+                    Console.WriteLine($"{multiplier} X {i} = {multiplier * i}");
+                }
+            }
+        }
+    }
+}
