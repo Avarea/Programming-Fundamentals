a2ee338b81ff385f4d56fbf9bcd479cb0d87d530
Avarea <valchek@abv.bg>
2017-05-29 22:58:36 +0300

    Conditional Statements and Loops - Problem 11. Odd Number

---
+++ ConditionalStatementsAndLoops/11OddNumber/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace _11OddNumber
+{
+    class Program
+    {
+        static void Main()
+        {
+
+            for (int i = 0; i < int.MaxValue; i++)
+            {
+                int num = Math.Abs(int.Parse(Console.ReadLine()));
+                if (num % 2 == 1)
+                {
+                    Console.WriteLine("The number is: {0}", num);
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Please write an odd number.");
+                }
+            }
+        }
+    }
+}
