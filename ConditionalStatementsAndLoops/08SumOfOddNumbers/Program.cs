aab5b5a5d77d2e4efa6a9c2a5bd3c8a7d4dd9c2c
Avarea <valchek@abv.bg>
2017-05-29 22:02:47 +0300

    Conditional Statements and Loops - Problem 8. Sum of Odd Numbers

---
+++ ConditionalStatementsAndLoops/08SumOfOddNumbers/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace _08SumOfOddNumbers
+{
+    class Program
+    {
+        static void Main()
+        {
+            int num = int.Parse(Console.ReadLine());
+            int sum = 0;
+            int count = 0;
+
+            for (int i = 1; ; i++)
+            {
+                if (i % 2 == 1)
+                {
+                    count++;
+                    sum += i;
+                    Console.WriteLine(i);
+                }
+
+                if (count == num)
+                {
+                    break;
+                }
+            }
+            Console.WriteLine($"Sum: {sum}");
+        }
+    }
+}
