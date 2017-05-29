c281bf893388b10739bfa370cd121d98796201bd
Avarea <valchek@abv.bg>
2017-05-29 20:19:31 +0300

    Conditional Statements and Loops - Problem 3. Back in 30 Minutes

---
+++ ConditionalStatementsAndLoops/03BackIn30Minutes/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace _03BackIn30Minutes
+{
+    class Program
+    {
+        static void Main()
+        {
+            int hours = int.Parse(Console.ReadLine());
+            int minutes = int.Parse(Console.ReadLine());
+
+            minutes += 30;
+
+            hours += minutes / 60;
+            minutes = minutes % 60;
+            if (hours >= 24)
+            {
+                hours = 0;
+
+            }
+            Console.WriteLine("{0}:{1:D2}", hours, minutes);
+        }
+    }
+}
