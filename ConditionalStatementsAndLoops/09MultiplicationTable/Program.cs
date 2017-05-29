9e2aad4a7564b7d0cba0557fadbd11662010cc61
Avarea <valchek@abv.bg>
2017-05-29 22:03:08 +0300

    Conditional Statements and Loops - Problem 9. Multiplication Table

---
+++ ConditionalStatementsAndLoops/09MultiplicationTable/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace _09MultiplicationTable
+{
+    class Program
+    {
+        static void Main()
+        {
+            int number = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= 10; i++)
+            {
+                Console.WriteLine($"{number} X {i} = {number * i}");
+            }
+
+        }
+    }
+}
