e6883cbafa1a0b4d70b60202aec0b53d3d1be7a0
Avarea <valchek@abv.bg>
2017-05-30 18:47:51 +0300

    C# Conditional statements and loops - exercises - 09 - Count the Integers

---
+++ ConditionalStatementsAndLoops/09CountTheIntegers/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace _09CountTheIntegers
+{
+    class Program
+    {
+        static void Main()
+        {
+
+            int count = 0;
+
+            for (int i = 1; i <= 100; i++)
+            {
+                try
+                {
+                    int num = int.Parse(Console.ReadLine());
+                    count++;
+                }
+                catch
+                {
+                    break;
+                }
+            }
+            Console.WriteLine(count);
+        }
+    }
+}
