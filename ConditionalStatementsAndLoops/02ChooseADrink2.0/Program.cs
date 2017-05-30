bb7040af781de7f6e25a30dd8fc73fb511c62f96
Avarea <valchek@abv.bg>
2017-05-30 18:45:57 +0300

    C# Conditional statements and loops - exercises - 02 - Choose a drink 2.0

---
+++ ConditionalStatementsAndLoops/02ChooseADrink2.0/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace _02ChooseADrink2._0
+{
+    class Program
+    {
+        static void Main()
+        {
+            string proffession = Console.ReadLine();
+            int quantity = int.Parse(Console.ReadLine());
+            double price = 0;
+
+            if (proffession == "Athlete")
+            {
+                price = 0.7;
+            }
+            else if (proffession == "Businessman" || proffession == "Businesswoman")
+            {
+                price = 1.00;
+            }
+            else if (proffession == "SoftUni Student")
+            {
+                price = 1.7;
+            }
+            else
+            {
+                price = 1.2;
+            }
+            Console.WriteLine("The {0} has to pay {1:f2}.", proffession, (price * quantity));
+        }
+    }
+}
