1f8596718cad47af47cac0d5d9dd8b24c125139e
Avarea <valchek@abv.bg>
2017-05-30 18:46:56 +0300

    C# Conditional statements and loops - exercises - 06 - Interval of Numbers

---
+++ ConditionalStatementsAndLoops/06IntervalOfNumbers/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace _06IntervalOfNumbers
+{
+    class Program
+    {
+        static void Main()
+        {
+            int firstNum = int.Parse(Console.ReadLine());
+            int secondNum = int.Parse(Console.ReadLine());
+            int temp = 0;
+
+            if (secondNum < firstNum)
+            {
+                temp = firstNum;
+                firstNum = secondNum;
+                secondNum = temp;
+            }
+            for (int i = firstNum; i <= secondNum; i++)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
