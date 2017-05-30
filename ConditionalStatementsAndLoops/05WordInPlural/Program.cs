71608a42b6ceb383e68bcdda3d27f9b51c54cc85
Avarea <valchek@abv.bg>
2017-05-30 18:46:41 +0300

    C# Conditional statements and loops - exercises - 05 - Word in Plural

---
+++ ConditionalStatementsAndLoops/05WordInPlural/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace _05WordInPlural
+{
+    class Program
+    {
+        static void Main()
+        {
+            string word = Console.ReadLine();
+
+            if (word.EndsWith("y") == true)
+            {
+                char y = 'y';
+                word = word.TrimEnd(y);
+                word += "ies";
+            }
+            else if (word.EndsWith("o") || word.EndsWith("ch") || word.EndsWith("s") || word.EndsWith("sh") || word.EndsWith("x") || word.EndsWith("z"))
+            {
+                word += "es";
+            }
+            else
+            {
+                word += "s";
+            }
+
+            Console.WriteLine(word);
+        }
+    }
+}
