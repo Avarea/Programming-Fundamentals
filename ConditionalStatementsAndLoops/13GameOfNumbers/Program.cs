1de20cb30366636ba38f180ea5d5a4b500e2ee99
Avarea <valchek@abv.bg>
2017-05-30 20:14:16 +0300

    Conditional Statements and Loops - Problem 13. Game of Numbers

---
+++ ConditionalStatementsAndLoops/13GameOfNumbers/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace _13GameOfNumbers
+{
+    class Program
+    {
+        static void Main()
+        {
+            int n = int.Parse(Console.ReadLine());
+            int m = int.Parse(Console.ReadLine());
+            int magicNum = int.Parse(Console.ReadLine());
+            int count = 0;
+
+            for (int i = n; i <= m; i++)
+            {
+                for (int j = n; j <= m; j++)
+                {
+                    count++;
+                    if (i + j == magicNum)
+                    {
+                        Console.WriteLine($"Number found! {j} + {i} = {magicNum}");
+                        return;
+                    }
+
+                }
+            }
+            Console.WriteLine($"{count} combinations - neither equals {magicNum}");
+
+        }
+    }
+}
