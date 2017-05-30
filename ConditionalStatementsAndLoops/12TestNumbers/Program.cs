d801580545333c914e71d4b671c9e4f4c22035fc
Avarea <valchek@abv.bg>
2017-05-30 20:00:31 +0300

    Conditional Statements and Loops - Problem 12. Test Numbers

---
+++ ConditionalStatementsAndLoops/12TestNumbers/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+
+namespace _12TestNumbers
+{
+    class Program
+    {
+        static void Main()
+        {
+            int n = int.Parse(Console.ReadLine());
+            int m = int.Parse(Console.ReadLine());
+            int maxSum = int.Parse(Console.ReadLine());
+            int count = 0;
+            int sum = 0;
+
+            for (int i = n; i >= 1; i--)
+            {
+                for (int j = 1; j <= m; j++)
+                {
+                    if (sum >= maxSum)
+                    {
+                        break;
+                    }
+                    count++;
+                    sum += (i * j) * 3;
+                }
+            }
+            if (maxSum > sum)
+            {
+                Console.WriteLine($"{count} combinations");
+                Console.WriteLine($"Sum: {sum}");
+            }
+            else
+            {
+                Console.WriteLine($"{count} combinations");
+                Console.WriteLine($"Sum: {sum} >= {maxSum}");
+            }
+
+        }
+    }
+}
