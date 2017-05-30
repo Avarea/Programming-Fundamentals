6c66bf3cb00609c8ac8bf65446e135a486e4f645
Avarea <valchek@abv.bg>
2017-05-30 18:47:16 +0300

    C# Conditional statements and loops - exercises - 07 - Cake Ingredients

---
+++ ConditionalStatementsAndLoops/07CakeIngredients/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace _07CakeIngredients
+{
+    class Program
+    {
+        static void Main()
+        {
+            int count = 0;
+            for (int i = 0; i <= 20; i++)
+            {
+                string ingredient = Console.ReadLine();
+                if (ingredient == "Bake!")
+                {
+                    Console.WriteLine("Preparing cake with {0} ingredients.", count);
+                    break;
+                }
+                count++;
+                Console.WriteLine("Adding ingredient {0}.", ingredient);
+            }
+        }
+    }
+}
