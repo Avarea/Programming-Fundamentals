1f0d5e9d5e5c10a0731100361ab92cc9b033fb41
Avarea <valchek@abv.bg>
2017-05-30 20:41:02 +0300

    Conditional Statements and Loops - Problem 14. * Magic Letter3

---
+++ ConditionalStatementsAndLoops/14MagicLetter/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace _14MagicLetter
+{
+    class Program
+    {
+        static void Main()
+        {
+            char firstLetter = char.Parse(Console.ReadLine());
+            char secondLetter = char.Parse(Console.ReadLine());
+            char magicLetter = char.Parse(Console.ReadLine());
+
+            for (char i = firstLetter; i <= secondLetter; i++)
+            {
+                if (i == magicLetter)
+                {
+                    continue;
+                }
+                for (char j = firstLetter; j <= secondLetter; j++)
+                {
+                    if (j == magicLetter)
+                    {
+                        continue;
+                    }
+                    for (char k = firstLetter; k <= secondLetter; k++)
+                    {
+                        if (k == magicLetter)
+                        {
+                            continue;
+                        }
+                        Console.Write($"{i}{j}{k} ");
+                    }
+                }
+            }
+            Console.WriteLine();
+        }
+    }
+}
