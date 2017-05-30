2bb68a5e3ce1ff6263c0f80354fde70a16d7c3d8
Avarea <valchek@abv.bg>
2017-05-30 18:45:46 +0300

    C# Conditional statements and loops - exercises - 01 - Choose a drink

---
+++ ConditionalStatementsAndLoops/01ChooseADrink/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace _01ChooseADrink
+{
+    class Program
+    {
+        static void Main()
+        {
+            string proffession = Console.ReadLine();
+            string output = string.Empty;
+
+            if (proffession == "Athlete")
+            {
+                output = "Water";
+            }
+            else if (proffession == "Businessman" || proffession == "Businesswoman")
+            {
+                output = "Coffee";
+            }
+            else if (proffession == "SoftUni Student")
+            {
+                output = "Beer";
+            }
+            else
+            {
+                output = "Tea";
+            }
+            Console.WriteLine(output);
+        }
+    }
+}
