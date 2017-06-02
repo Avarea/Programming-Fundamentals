c21f019e79eb9f1633c1d210dc5e304a0f907f64
Avarea <valchek@abv.bg>
2017-06-02 20:21:28 +0300

    Basic Syntax - More Exercises - 04 - Photo Gallery

---
+++ IntroAndSyntax/04PhotoGallery/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+namespace _04PhotoGallery
+{
+    class Program
+    {
+        static void Main()
+        {
+            int photoNumber = int.Parse(Console.ReadLine());
+            int day = int.Parse(Console.ReadLine());
+            int month = int.Parse(Console.ReadLine());
+            int year = int.Parse(Console.ReadLine());
+            int hours = int.Parse(Console.ReadLine());
+            int minutes = int.Parse(Console.ReadLine());
+            double size = double.Parse(Console.ReadLine());
+            int width = int.Parse(Console.ReadLine());
+            int height = int.Parse(Console.ReadLine());
+
+            double result = (width * height / 1000000);
+            double resolution = Math.Round(result, 1);
+
+            string dataType = "B";
+            if (size > 1000)
+            {
+                size = size / 1000;
+                dataType = "KB";
+
+                if (size >= 1000)
+                {
+                    size = size / 1000;
+                    dataType = "MB";
+
+                    if (size >= 1000)
+                    {
+                        size = size / 1000;
+                        dataType = "GB";
+                    }
+                }
+            }
+
+            string orientation = string.Empty;
+            if (width == height)
+            {
+                orientation = "square";
+            }
+            else if (width >= height)
+            {
+                orientation = "landscape";
+            }
+            else
+            {
+                orientation = "portrait";            }
+
+            Console.WriteLine("Name: DSC_{0:d4}.jpg", photoNumber);
+            Console.WriteLine($"Date Taken: {day:d2}/{month:d2}/{year} {hours:d2}:{minutes:d2}");
+            Console.WriteLine("Size: {0}{1}", size, dataType);
+            Console.WriteLine("Resolution: {0}x{1} ({2})", width, height, orientation);
+        }
+    }
+}
