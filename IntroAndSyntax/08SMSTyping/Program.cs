cc68522d3877480dbc41e32ea1344ed84f9b724a
Avarea <valchek@abv.bg>
2017-06-02 20:22:28 +0300

    Basic Syntax - More Exercises - 08 - SMS typing

---
+++ IntroAndSyntax/08SMSTyping/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace _08SMSTyping
+{
+    class Program
+    {
+        static void Main()
+        {
+            int charNum = int.Parse(Console.ReadLine());
+            string word = string.Empty;
+
+            for (int i = 1; i <= charNum; i++)
+            {
+                int integers = int.Parse(Console.ReadLine());
+
+
+                if (integers == 0)
+                {
+                    word += " ";
+                }
+                else
+                {
+                    int digitLenght = integers.ToString().Length;
+                    int mainDigit = 0;
+                    if (integers < 10)
+                    {
+                        mainDigit = integers;
+                    }
+                    else
+                    {
+                        while (integers >= 10)
+                        {
+                            integers /= 10;
+                            mainDigit = integers;
+                        }
+                    }
+
+
+                    int offsetNumber = (mainDigit - 2) * 3;
+
+                    if (mainDigit == 8 || mainDigit == 9)
+                    {
+                        offsetNumber++;
+                    }
+
+                    int letterIndex = (offsetNumber + digitLenght - 1);
+                    word += (char)(letterIndex + 97);
+                }
+
+            }
+            Console.WriteLine(word);
+        }
+    }
+}
