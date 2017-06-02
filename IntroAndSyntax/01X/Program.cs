d5e5f0853a1f43fcc6a883c9b14dba1f3aeb1ea3
Avarea <valchek@abv.bg>
2017-06-02 20:20:51 +0300

    Basic Syntax - More Exercises - 01 - X

---
+++ IntroAndSyntax/01X/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace _01X
+{
+    class Program
+    {
+        static void Main()
+        {
+            int n = int.Parse(Console.ReadLine());
+            int topSpaces = n - 2;
+            int topStart = 0;
+            for (int i = 1; i <= n / 2; i++)
+            {
+
+                Console.WriteLine("{0}x{1}x{0}", new string(' ', topStart), new string(' ', topSpaces));
+                topSpaces = topSpaces - 2;
+                topStart++;
+            }
+            Console.WriteLine("{0}x{0}", new string(' ', (n - 1) / 2));
+
+            int botSpaces = 1;
+            int botStart = (n - 3) / 2;
+
+            for (int i = 1; i <= n / 2; i++)
+            {
+                Console.WriteLine("{0}x{1}x{0}", new string(' ', botStart), new string(' ', botSpaces));
+                botSpaces += 2;
+                botStart--;
+            }
+        }
+    }
+}
