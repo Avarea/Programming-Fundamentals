c0189657cc494c561b7c1c7d6263cc650972e83a
Avarea <valchek@abv.bg>
2017-06-02 20:22:14 +0300

    Basic Syntax - More Exercises - 07 - Training Hall Equipment

---
+++ IntroAndSyntax/07TrainingHallEquipment/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace _07TrainingHallEquipment
+{
+    class Program
+    {
+        static void Main()
+        {
+            double budget = double.Parse(Console.ReadLine());
+            int items = int.Parse(Console.ReadLine());
+            double totalCost = 0;
+
+            for (int i = 1; i <= items; i++)
+            {
+                string itemName = Console.ReadLine();
+                double itemPrice = double.Parse(Console.ReadLine());
+                int itemCount = int.Parse(Console.ReadLine());
+                totalCost += itemPrice * itemCount;
+
+                if (itemCount > 1)
+                {
+                    itemName += "s";
+                    Console.WriteLine($"Adding {itemCount} {itemName} to cart.");
+                }
+                else
+                {
+                    Console.WriteLine($"Adding {itemCount} {itemName} to cart.");
+                }
+            }
+            if (budget >= totalCost)
+            {
+                Console.WriteLine("Subtotal: ${0:f2}", totalCost);
+                Console.WriteLine("Money left: ${0:f2}", budget - totalCost);
+            }
+            else
+            {
+                Console.WriteLine("Subtotal: ${0:f2}", totalCost);
+                Console.WriteLine("Not enough. We need ${0:f2} more.", Math.Abs(budget - totalCost));
+            }
+
+        }
+    }
+}
