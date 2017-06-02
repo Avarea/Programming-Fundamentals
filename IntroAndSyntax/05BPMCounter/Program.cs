ebbd0d2865885221db8a7a3b40eacf721fa4c9f2
Avarea <valchek@abv.bg>
2017-06-02 20:21:45 +0300

    Basic Syntax - More Exercises - 05 - BPM Counter

---
+++ IntroAndSyntax/05BPMCounter/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace _05BPMCounter
+{
+    class Program
+    {
+        static void Main()
+        {
+            int bpm = int.Parse(Console.ReadLine());
+            int beats = int.Parse(Console.ReadLine());
+
+            double bars = Math.Round(beats / 4.00, 1);
+
+
+            int minutes = beats / bpm;
+            double seconds = Math.Floor(60 * (beats / (double)bpm) - minutes * 60);
+
+
+            Console.WriteLine("{0} bars - {1}m {2}s", bars, minutes, seconds);
+        }
+    }
+}
