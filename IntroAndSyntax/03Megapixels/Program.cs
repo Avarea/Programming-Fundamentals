a6b7a30c981fbfb766701e6c03551130a33c4433
Avarea <valchek@abv.bg>
2017-06-02 20:21:15 +0300

    Basic Syntax - More Exercises - 03 - Megapixels

---
+++ IntroAndSyntax/03Megapixels/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace _03Megapixels
+{
+    class Program
+    {
+        static void Main()
+        {
+            double width = double.Parse(Console.ReadLine());
+            double height = double.Parse(Console.ReadLine());
+
+            double result = (width * height / 1000000);
+            Console.WriteLine("{0}x{1} => {2}MP",width, height, Math.Round(result, 1));
+            
+        }
+    }
+}
