44228d0a1b627f874b2a7abe090d0a1a3135469c
Avarea <valchek@abv.bg>
2017-06-06 16:33:41 +0300

    Exercises: Data Types and Variables - 17. ** Cypher Roulette

---
+++ DataTypes/17CypherRoulette/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace _17CypherRoulette
+{
+    class Program
+    {
+        static void Main()
+        {
+            int input = int.Parse(Console.ReadLine());
+            string result = string.Empty;
+            bool isReversed = false;
+            string lastChar = string.Empty;
+
+            for (int i = 1; i <= input; i++)
+            {
+
+                string currentString = Console.ReadLine();
+
+                if (currentString == lastChar)
+                {
+                    result = string.Empty;
+                    if (currentString == "spin")
+                    {
+                        input++;
+                    }
+                }
+
+                else
+                {
+                    if (currentString == "spin")
+                    {
+                        isReversed = !isReversed;
+                        input++;
+                    }
+                    else
+                    {
+                        if (isReversed)
+                        {
+                            result = currentString + result;
+                        }
+                        else
+                        {
+                            result = result + currentString;
+                        }
+                    }
+                }
+                lastChar = currentString;
+            }
+            Console.WriteLine(result);
+        }
+    }
+}
