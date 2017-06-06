f62e2983cedb27f8faf33c840addc0876e3426c1
Avarea <valchek@abv.bg>
2017-06-06 16:33:20 +0300

    Exercises: Data Types and Variables - 16. Tricky Strings

---
+++ DataTypes/16TrickyStrings/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace _16TrickyStrings
+{
+    class Program
+    {
+        static void Main()
+        {
+            string delimiter = Console.ReadLine();
+            int input = int.Parse(Console.ReadLine());
+            string result = string.Empty;
+
+            for (int i = 1; i <= input; i++)
+            {
+                string words = Console.ReadLine();
+                result += words;
+                if (i < input)
+                {
+                    result += delimiter;
+                }
+
+            }
+            result.TrimEnd();
+            Console.WriteLine(result.TrimEnd());
+        }
+    }
+}
