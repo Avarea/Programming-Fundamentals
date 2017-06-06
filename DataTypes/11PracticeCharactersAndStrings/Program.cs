f27e3bc256a054b6189df7b5ea003ac67ac867b5
Avarea <valchek@abv.bg>
2017-06-06 16:31:04 +0300

    Exercises: Data Types and Variables - 11. Practice Characters and Strings

---
+++ DataTypes/11PracticeCharactersAndStrings/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace _11PracticeCharactersAndStrings
+{
+    class Program
+    {
+        static void Main()
+        {
+            string softUni = "Software University";
+            char b = 'B';
+            char y = 'y';
+            char e = 'e';
+            string prog = "I love programming";
+
+            Console.WriteLine(softUni);
+            Console.WriteLine(b);
+            Console.WriteLine(y);
+            Console.WriteLine(e);
+            Console.WriteLine(prog);
+        }
+    }
+}
