beea38b2a381351eb3c0dde7e2ab11a60ccf93ef
Avarea <valchek@abv.bg>
2017-06-06 16:25:58 +0300

    Exercises: Data Types and Variables - 3. Exchange Variable Values

---
+++ DataTypes/03ExchangeVariableValues/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace _03ExchangeVariableValues
+{
+    class Program
+    {
+        static void Main()
+        {
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+            int temp = 0;
+
+            temp = a;
+            a = b;
+            b = temp;
+
+            Console.WriteLine(a);
+            Console.WriteLine(b);
+
+        }
+    }
+}
