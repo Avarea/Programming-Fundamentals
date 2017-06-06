48312831f7e531d1ba436baa5ea215d1dfb522e8
Avarea <valchek@abv.bg>
2017-06-06 16:26:42 +0300

    Exercises: Data Types and Variables - 5. Distance of The Stars

---
+++ DataTypes/05DistanceOfTheStars/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace _05DistanceOfTheStars
+{
+    class Program
+    {
+        static void Main()
+        {
+            decimal nearestStar = 4.22M;
+            decimal galaxy = 26000M;
+            decimal milkyWay = 100000M;
+            decimal universe = 46500000000M;
+
+            decimal lightYear = 9450000000000M;
+
+            Console.WriteLine("{0:e2}",nearestStar * lightYear);
+            Console.WriteLine("{0:e2}", galaxy * lightYear);
+            Console.WriteLine("{0:e2}", milkyWay * lightYear);
+            Console.WriteLine("{0:e2}", universe * lightYear);
+
+
+        }
+    }
+}
