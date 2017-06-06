cb286463d59725f37c25ed45ce771d44e888a3b3
Avarea <valchek@abv.bg>
2017-06-06 16:27:42 +0300

    Exercises: Data Types and Variables - 7. From Terabytes to Bits

---
+++ DataTypes/07FromTerabytesToBits/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace _07FromTerabytesToBits
+{
+    class Program
+    {
+        static void Main()
+        {
+            double input = double.Parse(Console.ReadLine());
+
+            Console.WriteLine(input * 8796093022208);
+        }
+    }
+}
