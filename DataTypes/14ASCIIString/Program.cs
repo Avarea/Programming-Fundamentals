24b6fc5f3a9b580d4c49be8e9d67fe9b437b3fbc
Avarea <valchek@abv.bg>
2017-06-06 16:32:35 +0300

    Exercises: Data Types and Variables - 14. ASCII String

---
+++ DataTypes/14ASCIIString/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace _14ASCIIString
+{
+    class Program
+    {
+        static void Main()
+        {
+            int n = int.Parse(Console.ReadLine());
+            string output = string.Empty;
+
+            for (int i = 1; i <= n; i++)
+            {
+                int character = int.Parse(Console.ReadLine());
+                output += (char)character;
+            }
+            Console.WriteLine(output);
+
+
+        }
+    }
+}
