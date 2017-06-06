5dab639bb9a77c5b966c3fa6f8efd873959c54de
Avarea <valchek@abv.bg>
2017-06-06 16:28:07 +0300

    Exercises: Data Types and Variables - 8. Traveling at Light Speed

---
+++ DataTypes/08TravelingAtLightSpeed/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Globalization;
+
+namespace _08TravelingAtLightSpeed
+{
+    class Program
+    {
+        static void Main()
+        {
+            decimal input = decimal.Parse(Console.ReadLine());
+            decimal lightYear = 9450000000000;
+            decimal lightSpeed = 300000;
+
+            decimal total = (lightYear / lightSpeed) * input;
+
+
+
+            TimeSpan diff = TimeSpan.FromSeconds((double)total);
+            string formatted = string.Format(
+                  CultureInfo.CurrentCulture,
+                  "{0} weeks\n{1} days\n{2} hours\n{3} minutes\n{4} seconds\n",
+                  diff.Days / 7,
+                  diff.Days % 7,
+                  diff.Hours,
+                  diff.Minutes,
+                  diff.Seconds);
+
+            Console.WriteLine(formatted);
+        }
+    }
+}
---
+++ DataTypes/08TravelingAtLightSpeed/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("08TravelingAtLightSpeed")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("08TravelingAtLightSpeed")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("05f2fd4b-ea8a-44fd-ad05-c50693b70803")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
