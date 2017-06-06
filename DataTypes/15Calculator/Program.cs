e30dc5d9d7b5609c1781c96fa90cf3d07ed33040
Avarea <valchek@abv.bg>
2017-06-06 16:32:56 +0300

    Exercises: Data Types and Variables - 15. Calculator

---
+++ DataTypes/15Calculator/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace _15Calculator
+{
+    class Program
+    {
+        static void Main()
+        {
+            int num1 = int.Parse(Console.ReadLine());
+            string operatorR = Console.ReadLine();
+            int num2 = int.Parse(Console.ReadLine());
+
+            switch (operatorR)
+            {
+                case "+":
+                    Console.WriteLine($"{num1} + {num2} = {num1 + num2}");
+                    break;
+                case "-":
+                    Console.WriteLine($"{num1} - {num2} = {num1 - num2}");
+                    break;
+                case "*":
+                    Console.WriteLine($"{num1} * {num2} = {num1 * num2}");
+                    break;
+                case "/":
+                    Console.WriteLine($"{num1} / {num2} = {num1 / num2}");
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+}
