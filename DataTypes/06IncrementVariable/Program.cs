b0aac9477b5b6adb0ff66887b9b62f32134d3b90
Avarea <valchek@abv.bg>
2017-06-06 16:27:04 +0300

    Exercises: Data Types and Variables - 6. Increment Variable

---
+++ DataTypes/06IncrementVariable/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace _06IncrementVariable
+{
+    class Program
+    {
+        static void Main()
+        {
+            int number = int.Parse(Console.ReadLine());
+
+            int overflow = number / 256;
+            int result = number % 256;
+
+            Console.WriteLine(result);
+            Console.WriteLine("Overflowed {0} times", overflow);
+
+             
+
+        }
+    }
+}
