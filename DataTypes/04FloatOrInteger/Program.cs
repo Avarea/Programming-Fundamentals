5277121ff0c7780f67ff8804ea65a4dca7345dd5
Avarea <valchek@abv.bg>
2017-06-06 16:26:16 +0300

    Exercises: Data Types and Variables - 2. Float or Integer

---
+++ DataTypes/04FloatOrInteger/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace _04FloatOrInteger
+{
+    class Program
+    {
+        static void Main()
+        {
+            double num = double.Parse(Console.ReadLine());
+
+            Console.WriteLine(Math.Round(num));
+        }
+    }
+}
