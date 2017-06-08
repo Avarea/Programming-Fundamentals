c41a6107c01cbe88bf707b44add1a78356419013
Avarea <valchek@abv.bg>
2017-06-08 20:26:57 +0300

    Lab: Data Types: Numeral Types and Type Conversion - 6. Triples of Latin Letters

---
+++ DataTypes/06TriplesOfLatinLetters/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+
+namespace _06TriplesOfLatinLetters
+{
+    class Program
+    {
+        static void Main()
+        {
+            int n = int.Parse((Console.ReadLine()));
+
+
+            for (int i = 0; i <= n - 1; i++)
+            {
+                for (int j = 0; j <= n - 1; j++)
+                {
+                    for (int k = 0; k <= n - 1; k++)
+                    {
+                        Console.WriteLine($"{(char)(i + 'a')}{(char)(j + 'a')}{(char)(k + 'a')}");
+                    }
+                }
+            }
+        }
+    }
+}
