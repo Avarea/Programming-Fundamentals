7042997c04e7dd83be15af306a0cc9f3b1e03723
Avarea <valchek@abv.bg>
2017-06-08 20:26:21 +0300

    Lab: Data Types: Numeral Types and Type Conversion - 4. Transport

---
+++ DataTypes/04Transport/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace _04Transport
+{
+    class Program
+    {
+        static void Main()
+        {
+            int numberOfPersons = int.Parse(Console.ReadLine());
+            double capacity = 24;
+
+            Console.WriteLine(Math.Ceiling((numberOfPersons / capacity)));
+        }
+    }
+}
