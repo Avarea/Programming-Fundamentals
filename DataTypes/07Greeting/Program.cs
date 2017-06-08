9480d7c57ba01bced73c3bcdf840616530aaffc6
Avarea <valchek@abv.bg>
2017-06-08 20:27:19 +0300

    Lab: Data Types: Numeral Types and Type Conversion - 7. Greeting

---
+++ DataTypes/07Greeting/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace _07Greeting
+{
+    class Program
+    {
+        static void Main()
+        {
+            string fName = Console.ReadLine();
+            string lName = Console.ReadLine();
+            int age = int.Parse(Console.ReadLine());
+
+            Console.WriteLine($"Hello, {fName} {lName}. You are {age} years old.");
+        }
+    }
+}
