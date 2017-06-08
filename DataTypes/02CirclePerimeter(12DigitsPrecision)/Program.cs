e4404efc1fac97509c7bfb0d6df964fe244fd1fa
Avarea <valchek@abv.bg>
2017-06-08 20:25:50 +0300

    Lab: Data Types: Numeral Types and Type Conversion - 2. Circle Perimeter (12 Digits Precision)

---
+++ DataTypes/02CirclePerimeter(12DigitsPrecision)/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace _02CirclePerimeter_12DigitsPrecision_
+{
+    class Program
+    {
+        static void Main()
+        {
+            double radius = double.Parse(Console.ReadLine());
+            Console.WriteLine("{0:f12}", 2 * Math.PI * radius);
+        }
+    }
+}
