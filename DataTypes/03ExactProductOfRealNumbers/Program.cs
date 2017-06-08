b47e24e223665075692b979307a85ec321c38f28
Avarea <valchek@abv.bg>
2017-06-08 20:26:07 +0300

    Lab: Data Types: Numeral Types and Type Conversion - 3. Exact Product of Real Numbers

---
+++ DataTypes/03ExactProductOfRealNumbers/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace _03ExactProductOfRealNumbers
+{
+    class Program
+    {
+        static void Main()
+        {
+            int input = int.Parse(Console.ReadLine());
+            decimal product = 1;
+
+            for (int i = 0; i < input; i++)
+            {
+                decimal num1 = decimal.Parse((Console.ReadLine()));
+                product *= num1;
+            }
+            Console.WriteLine(product);
+        }
+    }
+}
