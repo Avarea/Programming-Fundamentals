fb7459f5c12a8ae8658731fc03bf76bc7f2d17a0
Avarea <valchek@abv.bg>
2017-06-10 22:12:16 +0300

    Lab: Methods, Debugging and Troubleshooting Code - 4. Draw a Filled Square

---
+++ MethodsAndDebugging/04DrawAFilledSquare/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace _04DrawAFilledSquare
+{
+    class Program
+    {
+        static void Main()
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            PrintSide(n);
+            PrintBody(n);
+            PrintSide(n);
+        }
+
+         static void PrintBody(int n)
+        {
+            for (int i = 0; i < n - 2; i++)
+            {
+                Console.Write('-');
+                for (int j = 1; j < n; j++)
+                {
+                    Console.Write("\\/");
+                }
+                Console.WriteLine('-');
+            }
+
+        }
+
+        static void PrintSide(int n)
+        {
+            Console.WriteLine(new string('-', n * 2));
+        }
+    }
+}
