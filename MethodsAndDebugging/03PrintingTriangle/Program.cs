4df13ec2d57b6ce51bc1dc9e5524da47bf7157b1
Avarea <valchek@abv.bg>
2017-06-10 22:11:50 +0300

    Lab: Methods, Debugging and Troubleshooting Code - 3. Printing Triangle

---
+++ MethodsAndDebugging/03PrintingTriangle/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace _03PrintingTriangle
+{
+    class Program
+    {
+        static void Main()
+        {
+            int n = int.Parse(Console.ReadLine());
+            int start = 1;
+            int end = n;
+
+
+
+            for (int i = start; i < n; i++)
+            {
+                PrintLine(1, i);
+            }
+            PrintLine(1, n);
+
+            for (int j = end - 1; j >= 0; j--)
+            {
+                PrintLine(1, j);
+            }
+        }
+
+
+
+        static void PrintLine(int start, int end)
+        {
+            for (int i = start; i <= end; i++)
+            {
+                Console.Write(i + " ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
