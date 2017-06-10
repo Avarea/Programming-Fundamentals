9be055ab529ab63c6677d2b337c1da66f64991be
Avarea <valchek@abv.bg>
2017-06-10 22:12:54 +0300

    Lab: Methods, Debugging and Troubleshooting Code - 6. Math Power

---
+++ MethodsAndDebugging/06MathPower/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace _06MathPower
+{
+    class Program
+    {
+        static void Main()
+        {
+            double number = double.Parse(Console.ReadLine());
+            int power = int.Parse(Console.ReadLine());
+
+
+            Console.WriteLine(GetPower(number, power));
+       
+            
+        }
+
+         static double GetPower(double number, int power)
+         {
+             return Math.Pow(number, power);
+         }
+    }
+}
