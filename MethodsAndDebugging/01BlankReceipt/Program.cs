a3d800c5a01df44ffbeae43daf663aea95d65550
Avarea <valchek@abv.bg>
2017-06-10 22:11:15 +0300

    Lab: Methods, Debugging and Troubleshooting Code - 1. Blank Receipt

---
+++ MethodsAndDebugging/01BlankReceipt/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01BlankReceipt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PrintReceipt();
+
+        }
+
+         static void PrintReceipt()
+        {
+            PrintReceiptHeader();
+            PrintReceiptBody();
+            PrintReceiptFooter();
+        }
+
+        static void PrintReceiptFooter()
+        {
+            Console.WriteLine("------------------------------");
+            Console.WriteLine("© SoftUni");
+        }
+
+        static void PrintReceiptBody()
+        {
+            Console.WriteLine("Charged to____________________");
+            Console.WriteLine("Received by___________________");
+        }
+
+        static void PrintReceiptHeader()
+        {
+            Console.WriteLine("CASH RECEIPT");
+            Console.WriteLine("------------------------------");
+        }
+    }
+}
