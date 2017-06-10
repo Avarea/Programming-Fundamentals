93e05dde46adcf9de934f4356909d8059be35241
Avarea <valchek@abv.bg>
2017-06-10 22:13:49 +0300

    Lab: Methods, Debugging and Troubleshooting Code - 8. Multiply Evens by Odds

---
+++ MethodsAndDebugging/08MultiplyEvensByOdds/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace _08MultiplyEvensByOdds
+{
+    class Program
+    {
+        static void Main()
+        {
+            int n = Math.Abs(int.Parse(Console.ReadLine()));
+
+            GetMultipleOfEvensAndOdds(n);
+            Console.WriteLine(GetMultipleOfEvensAndOdds(n));
+        }
+
+         static int GetMultipleOfEvensAndOdds(int n)
+         {
+             int sumEvens = GetSumOfEvenDigits(n);
+             int sumOdds = GetSumOfOddDigits(n);
+             return sumOdds * sumEvens;
+         }
+
+         static int GetSumOfEvenDigits(int n)
+         {
+             int sum = 0;
+             while (n > 0)
+             {
+                 int lastDigit = n % 10;
+                 if (lastDigit % 2 == 0)
+                 {
+                     sum += lastDigit;
+                 }
+                 n /= 10;
+             }
+             return sum;
+         }
+
+        static int GetSumOfOddDigits(int n)
+        {
+            int sum = 0;
+            while (n > 0)
+            {
+                int lastDigit = n % 10;
+                if (lastDigit % 2 != 0)
+                {
+                    sum += lastDigit;
+                }
+                n /= 10;
+            }
+            return sum;
+        }
+    }
+}
