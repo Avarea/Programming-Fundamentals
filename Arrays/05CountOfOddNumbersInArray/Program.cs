e5c203b95d691864deef214118cd82266300d19c
Avarea <valchek@abv.bg>
2017-06-11 12:12:56 +0300

    Lab: Arrays: Simple Array Processing - 5. Count of Odd Numbers in Array

---
+++ Arrays/05CountOfOddNumbersInArray/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq;
+
+
+namespace _05CountOfOddNumbersInArray
+{
+    class Program
+    {
+        static void Main()
+        {
+            double[] numbers = Console.ReadLine().Split(' ').Select(double.Parse).ToArray();
+
+            int count = 0;
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                if (numbers[i] % 2 == 1 || numbers[i] % 2 == -1)
+                {
+                    count++;
+                }
+            }
+
+            Console.WriteLine(count);
+        }
+    }
+}
