065d69ba7a84c5a1121355e59b339e0d1fa1e124
Avarea <valchek@abv.bg>
2017-06-11 12:12:35 +0300

    Lab: Arrays: Simple Array Processing - 4. Rotate Array of Strings

---
+++ Arrays/04RotateArrayOfStrings/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Linq;
+
+namespace _04RotateArrayOfStrings
+{
+    class Program
+    {
+        static void Main()
+        {
+            var array = Console.ReadLine().Split(' ').ToArray();
+            var rotated = new string[array.Length];
+
+            for (int i = 0; i < array.Length - 1; i++)
+            {
+              
+                rotated[i + 1] = array[i];
+                var lastElement = array[rotated.Length - 1];
+                rotated[0] = lastElement;
+
+            }
+
+            Console.WriteLine(string.Join(" ", rotated));
+        }
+    }
+}
