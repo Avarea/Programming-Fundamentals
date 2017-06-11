aea8c971db55d8c6f70dc9a6b4fee4af28ddc01e
Avarea <valchek@abv.bg>
2017-06-11 12:12:13 +0300

    Lab: Arrays: Simple Array Processing - 3. Smallest Element in Array

---
+++ Arrays/03SmallestElementInArray/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Linq;
+
+
+namespace _03SmallestElementInArray
+{
+    class Program
+    {
+        static void Main()
+        {
+            int[] numbers = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+            int min = int.MaxValue;
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                if (numbers[i] < min)
+                {
+                    min = numbers[i];
+                }
+
+            }
+            Console.WriteLine(min);
+        }
+    }
+}
