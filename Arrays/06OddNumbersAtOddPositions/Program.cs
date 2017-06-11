a4bc5005a8648726d964de022e2cd8f8561463ea
Avarea <valchek@abv.bg>
2017-06-11 12:13:17 +0300

    Lab: Arrays: Simple Array Processing - 6. Odd Numbers at Odd Positions

---
+++ Arrays/06OddNumbersAtOddPositions/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Linq;
+
+
+namespace _06OddNumbersAtOddPositions
+{
+    class Program
+    {
+        static void Main()
+        {
+            double[] numbers = Console.ReadLine().Split(' ').Select(double.Parse).ToArray();
+
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                if (i % 2 == 1)
+                {
+                    if (numbers[i] % 2 == 1 || numbers[i] % 2 == -1)
+                    {
+                        Console.WriteLine("Index {0} -> {1}", i, numbers[i]);
+                    }
+                }
+
+            }
+
+        }
+    }
+}
