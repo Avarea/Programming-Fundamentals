9cd567afed776c3828c2dd8e04dc364ca7dfe4cc
Avarea <valchek@abv.bg>
2017-06-11 12:11:53 +0300

    Lab: Arrays: Simple Array Processing - 2. Multiply an Array of Doubles

---
+++ Arrays/02MultiplyAnArrayOfDoubles/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq;
+
+
+namespace _02MultiplyAnArrayOfDoubles
+{
+    class Program
+    {
+        static void Main()
+        {
+            double[] numbers = Console.ReadLine().Split(' ').Select(double.Parse).ToArray();
+            double p = double.Parse(Console.ReadLine());
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+
+                Console.Write(numbers[i] * p + " ");
+            }
+
+        }
+    }
+}
