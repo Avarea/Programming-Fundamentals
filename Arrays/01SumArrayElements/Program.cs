cca7e45c23c29a97177d1b8bb2d0d5b7825cd5b8
Avarea <valchek@abv.bg>
2017-06-11 12:11:34 +0300

    Lab: Arrays: Simple Array Processing - 1. Sum Array Elements

---
+++ Arrays/01SumArrayElements/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Linq;
+
+namespace _01SumArrayElements
+{
+    class Program
+    {
+        static void Main()
+        {
+            //int[] numbers = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+
+            int input = int.Parse(Console.ReadLine());
+            var array = new int[input];
+
+
+            int sum = 0;
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = int.Parse(Console.ReadLine());
+            }
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                sum += array[i];
+
+            }
+            Console.WriteLine(sum);
+        }
+    }
+}
