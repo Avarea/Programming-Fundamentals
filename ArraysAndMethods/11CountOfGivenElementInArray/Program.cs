f121e50bff1977c6af990e8ee67fdf9ec571caed
Avarea <valchek@abv.bg>
2017-06-13 20:52:52 +0300

    Exercises: Arrays and Methods - 11. Count of Given Element in Array

---
+++ ArraysAndMethods/11CountOfGivenElementInArray/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq;
+
+namespace _11CountOfGivenElementInArray
+{
+    class Program
+    {
+        static void Main()
+        {
+            int[] numbers = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+            int element = int.Parse(Console.ReadLine());
+            int count = 0;
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                if (numbers[i] == element)
+                {
+                    count++;
+                }
+            }
+            Console.WriteLine(count);
+        }
+    }
+}
