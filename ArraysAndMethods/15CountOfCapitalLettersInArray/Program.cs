e1d7f46c7f82e12aeab512c5d064dac11f950bfd
Avarea <valchek@abv.bg>
2017-06-13 21:42:32 +0300

    Exercises: Arrays and Methods - 15. Count of Capital Letters in Array

---
+++ ArraysAndMethods/15CountOfCapitalLettersInArray/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Linq;
+
+namespace _15CountOfCapitalLettersInArray
+{
+    class Program
+    {
+        static void Main()
+        {
+            var text = Console.ReadLine().Split(new char[] { ' ' },StringSplitOptions.RemoveEmptyEntries).ToArray();
+            int count = 0;
+
+            var singleLeters = text.Where(s => s.Length == 1).ToArray();
+
+
+
+            foreach (var letter in singleLeters)
+            {
+                var chars = char.Parse(letter);
+
+                if (chars >= 65 && chars <= 90)
+                {
+                    count++;
+                }
+            }
+
+            Console.WriteLine(count);
+        }
+    }
+}
