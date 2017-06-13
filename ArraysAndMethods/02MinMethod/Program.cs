4a146c1f3e82f22d7518cd68957b3cd970c5c6b6
Avarea <valchek@abv.bg>
2017-06-13 13:14:35 +0300

    Exercises: Arrays and Methods - 2. Min Method

---
+++ ArraysAndMethods/02MinMethod/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace _02MinMethod
+{
+    class Program
+    {
+        static void Main()
+        {
+            int num1 = int.Parse(Console.ReadLine());
+            int num2 = int.Parse(Console.ReadLine());
+            int num3 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(GetMin(num1, GetMin(num2, num3)));
+
+
+        }
+
+        static int GetMin(int num1, int num2)
+        {
+            if (num1 > num2)
+            {
+                return num2;
+            }
+            else
+            {
+                return num1;
+            }
+            
+
+        }
+    }
+}
