2897f37189fc8866f8155dc8a176098aea3ab5f9
Avarea <valchek@abv.bg>
2017-06-13 20:51:51 +0300

    Exercises: Arrays and Methods - 9. Largest Element in Array

---
+++ ArraysAndMethods/09LargestElementInArray/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _09LargestElementInArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            var array = new int[n];
+
+            for (int i = 0; i < n; i++)
+            {
+                array[i] = int.Parse(Console.ReadLine());
+            }
+
+            int largest = int.MinValue;
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (largest < array[i])
+                {
+                    largest = array[i];
+                }
+            }
+            Console.WriteLine(largest);
+        }
+    }
+}
