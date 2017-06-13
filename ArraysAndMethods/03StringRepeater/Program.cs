ca8990fbf240b3f29387ce1680e06e4510ea6e8d
Avarea <valchek@abv.bg>
2017-06-13 13:15:14 +0300

    Exercises: Arrays and Methods - 3. String Repeater

---
+++ ArraysAndMethods/03StringRepeater/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace _03StringRepeater
+{
+    class Program
+    {
+        static void Main()
+        {
+            string str = Console.ReadLine();
+            int count = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(RepeatString(str, count));
+        }
+
+        static string RepeatString(string str, int count)
+        {
+            string repeatedString = string.Empty;
+
+            for (int i = 0; i < count; i++)
+            {
+                repeatedString += str;
+            }
+
+            return repeatedString;
+        }
+    }
+}
