c05323ae529e86138824da2d3c06c386e2284669
Avarea <valchek@abv.bg>
2017-06-13 13:15:33 +0300

    Exercises: Arrays and Methods - 4. Nth Digit

---
+++ ArraysAndMethods/04NthDigit/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace _04NthDigit
+{
+    class Program
+    {
+        static void Main()
+        {
+            long number = long.Parse(Console.ReadLine());
+            int index = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(FindNthDigit(number, index));
+
+        }
+
+        static int FindNthDigit(long number, int index)
+        {
+            int CurrentIndex = 1;
+
+            while (number > 0)
+            {
+                if (index == CurrentIndex)
+                {
+                    return (int)number % 10;
+                }
+                else
+                {
+                    number = number / 10;
+                }
+                CurrentIndex++;
+            }
+            return (int)number;
+        }
+    }
+}
