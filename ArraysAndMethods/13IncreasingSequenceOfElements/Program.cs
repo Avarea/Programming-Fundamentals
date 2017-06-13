fa211e024443bac41794f3bdd5cc01a037a2090d
Avarea <valchek@abv.bg>
2017-06-13 20:53:44 +0300

    Exercises: Arrays and Methods - 13. Increasing Sequence of Elements

---
+++ ArraysAndMethods/13IncreasingSequenceOfElements/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Linq;
+
+
+namespace _13IncreasingSequenceOfElements
+{
+    class Program
+    {
+        static void Main()
+        {
+            int[] numbers = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+            bool isIncreasing = false;
+            int lastElement = 0;
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+
+                if (i == 0)
+                {
+                    lastElement = numbers[i];
+                }
+                else 
+                {
+                    if (numbers[i] > lastElement)
+                    {
+                        isIncreasing = true;
+                        lastElement = numbers[i];
+                    }
+                    else
+                    {
+                        isIncreasing = false;
+                        break;
+                    }
+                }
+            }
+            if (isIncreasing == true)
+            {
+                Console.WriteLine("Yes");
+            }
+            else
+            {
+                Console.WriteLine("No");
+            }
+        }
+    }
+}
