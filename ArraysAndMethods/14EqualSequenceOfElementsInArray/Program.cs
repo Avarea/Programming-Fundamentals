f0465b7d8f599847a9ad48607d77335a8cb8af61
Avarea <valchek@abv.bg>
2017-06-13 20:54:14 +0300

    Exercises: Arrays and Methods - 14. Equal Sequence of Elements in Array

---
+++ ArraysAndMethods/14EqualSequenceOfElementsInArray/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Linq;
+
+
+namespace _14EqualSequenceOfElementsInArray
+{
+    class Program
+    {
+        static void Main()
+        {
+            int[] numbers = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+            int lastElement = numbers[0];
+            bool areEqual = false;
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                if (numbers[i] == lastElement)
+                {
+                    areEqual = true;
+                    lastElement = numbers[i];
+                }
+                else
+                {
+                    areEqual = false;
+                    break;
+                }
+                
+            }
+            if (areEqual == true)
+            {
+                Console.WriteLine("Yes");
+            }
+            else
+            {
+                Console.WriteLine("No");
+            }
+        }
+    }
+}
