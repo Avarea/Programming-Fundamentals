3707e222f5fb3ec9a487470eccaaf39c65236b1e
Avarea <valchek@abv.bg>
2017-06-13 13:14:17 +0300

    Exercises: Arrays and Methods - 1. Hello, Name!

---
+++ ArraysAndMethods/01HelloName/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace _01HelloName
+{
+    class Program
+    {
+        static void Main()
+        {
+            string name = Console.ReadLine();
+
+            GetName(name);
+        }
+
+         static void GetName(string name)
+        {
+            Console.WriteLine($"Hello, {name}!");
+        }
+    }
+}
