27a5528dd91e112f26fdc862129743ff2ad8cfea
Avarea <valchek@abv.bg>
2017-06-13 20:53:15 +0300

    Exercises: Arrays and Methods - 12. Count Occurrences of Larger Numbers in Array

---
+++ ArraysAndMethods/12CountOccurrencesOfLargerNumsInArray/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Linq;
+
+
+namespace _12CountOccurrencesOfLargerNumsInArray
+{
+    class Program
+    {
+        static void Main()
+        {
+            double[] numbers = Console.ReadLine().Split(' ').Select(double.Parse).ToArray();
+            double element = double.Parse(Console.ReadLine());
+            int count = 0;
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                if (numbers[i] > element)
+                {
+                    count++;
+                }
+            }
+            Console.WriteLine(count);
+        }
+    }
+}
