47b65350b7613e16530e5a7e9bbc02870a31f8a8
Avarea <valchek@abv.bg>
2017-06-15 19:40:04 +0300

    Exercises: Arrays and Methods - 16. Array Symmetry

---
+++ ArraysAndMethods/16ArraySymmetry/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Linq;
+
+namespace _16ArraySymmetry
+{
+    class Program
+    {
+        static void Main()
+        {
+            var array = Console.ReadLine().Split(' ').ToArray();
+            var firstElement = array[0];
+            bool isEqual = false;
+            if (array[array.Length -1] == firstElement)
+            {
+                isEqual = true;
+            }
+            else
+            {
+                Console.WriteLine("No");
+                return;
+            }
+            if (isEqual = true)
+            {
+                Console.WriteLine("Yes");
+            }
+            else
+            {
+                Console.WriteLine("No");
+            }
+        }
+    }
+}
