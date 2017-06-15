f3678e93c9c0e4b6781aca85e8b6a4849b488b09
Avarea <valchek@abv.bg>
2017-06-15 19:40:24 +0300

    Exercises: Arrays and Methods - 17. * Altitude

---
+++ ArraysAndMethods/17Altitude/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+
+namespace _17Altitude
+{
+    class Program
+    {
+        static void Main()
+        {
+            var array = Console.ReadLine().Split(new char[] { ' ' },StringSplitOptions.RemoveEmptyEntries).ToList();
+            var initialAltitude = double.Parse(array[0]);
+            string result = string.Empty;
+            bool isCrashed = false;
+
+            for (int i = 1; i < array.Count; i+= 2)
+            {
+                var command = array[i];
+                var number = double.Parse(array[i + 1]);
+
+                switch (command)
+                {
+                    case "up":
+                        initialAltitude += number;
+                        break;
+                    case "down":
+                        initialAltitude -= number;
+                        break;
+                }
+
+                if (initialAltitude <= 0)
+                {
+                    result = "crashed";
+                    isCrashed = true;
+                    break;
+                }
+                if (!isCrashed)
+                {
+                    result = $"got through safely. current altitude: {initialAltitude}m";
+                }
+            }
+            Console.WriteLine(result);
+        }
+    }
+}
