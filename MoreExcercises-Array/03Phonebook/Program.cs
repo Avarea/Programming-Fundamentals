f70401bbfbec44f8cd2ad7e603a162a21e357a7e
Avarea <valchek@abv.bg>
2017-06-17 12:21:09 +0300

    More Exercises: Arrays - 3. Phonebook

---
+++ MoreExcercises-Array/03Phonebook/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _03Phonebook
+{
+    class Program
+    {
+        static void Main()
+        {
+            var phones = Console.ReadLine().Split(' ').ToArray();
+            var names = Console.ReadLine().Split(' ').ToArray();
+            string command = Console.ReadLine();
+
+            while (command != "done")
+            {
+                for (int j = 0; j < phones.Length; j++)
+                {
+                    if (command == names[j])
+                    {
+                        Console.WriteLine($"{names[j]} -> {phones[j]}");
+                    }
+                }
+                command = Console.ReadLine();
+
+            }
+
+
+
+
+        }
+    }
+}
