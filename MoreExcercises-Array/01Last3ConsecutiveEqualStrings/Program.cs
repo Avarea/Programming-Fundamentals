8a5a20fc4f6e5b910b746565ab38d72a0e70a8bf
Avarea <valchek@abv.bg>
2017-06-17 12:19:45 +0300

    More Exercises: Arrays - 1. Last 3 Consecutive Equal Strings

---
+++ MoreExcercises-Array/01Last3ConsecutiveEqualStrings/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Linq;
+
+
+namespace _01Last3ConsecutiveEqualStrings
+{
+    class Program
+    {
+        static void Main()
+        {
+            var array = Console.ReadLine().Split(' ').ToArray();
+
+            string firstString = String.Empty;
+            string secondString = String.Empty;
+            string thirdString = String.Empty;
+
+            thirdString = array[array.Length - 1];
+            secondString = array[array.Length - 2];
+            firstString = array[array.Length - 3];
+
+            for (int i = array.Length; i >= 0; i--)
+            {
+                if (thirdString == secondString && secondString == firstString)
+                {
+                    Console.WriteLine($"{firstString} {secondString} {thirdString}");
+                    return;
+                }
+                else
+                {
+                    
+                    thirdString = array[i - 1];
+                    secondString = array[i - 2];
+                    firstString = array[i - 3];
+                }
+            }
+
+
+
+        }
+    }
+}
