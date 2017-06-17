371bb77ccdb4933c2f8d5c5d490037e5504bb564
Avarea <valchek@abv.bg>
2017-06-17 12:20:25 +0300

    More Exercises: Arrays - 2. Array Elements Equal to Their Index

---
+++ MoreExcercises-Array/02ArrayElementsEqualToTheirIndex/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02ArrayElementsEqualToTheirIndex
+{
+    class Program
+    {
+        static void Main()
+        {
+            int[] numbers = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                if (i == numbers[i])
+                {
+                    Console.Write("{0} ",numbers[i]);
+                }
+            }
+            Console.WriteLine();
+        }
+    }
+}
