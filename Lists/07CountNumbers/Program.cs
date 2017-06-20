ab870e076ab0590bb872196add8da16c21bd5a0f
Avarea <valchek@abv.bg>
2017-06-20 19:37:46 +0300

    Lists - 7.Count Numbers using Lists

---
+++ Lists/07CountNumbers/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+
+namespace _07CountNumbersWihLists
+{
+    class Program
+    {
+        static void Main()
+        {
+            List<int> numbers = Console.ReadLine().Split(' ').Select(int.Parse).ToList();
+            numbers.Add(int.MaxValue);
+            numbers.Sort();
+
+            int count = 1;
+            for (int i = 0; i < numbers.Count - 1; i++)
+            {
+                if (numbers[i] == numbers[i + 1])
+                {
+                    count++;
+                }
+                else
+                {
+                    Console.WriteLine($"{numbers[i]} -> {count}");
+                    count = 1;
+                }
+            }
+        }
+    }
+}
