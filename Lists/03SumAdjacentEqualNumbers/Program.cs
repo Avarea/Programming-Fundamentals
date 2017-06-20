948ecb21b0c47f25640c7a35631eebdd867596f9
Avarea <valchek@abv.bg>
2017-06-20 16:31:46 +0300

    Lists - 03.Sum Adjacent Equal Numbers

---
+++ Lists/03SumAdjacentEqualNumbers/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+
+namespace _03SumAdjacentEqualNumbers
+{
+    class Program
+    {
+        static void Main()
+        {
+            List<decimal> numbers = Console.ReadLine().Split(' ').Select(decimal.Parse).ToList();
+            numbers.Add(decimal.MaxValue);
+
+            for (int i = 0; i < numbers.Count - 1; i++)
+            {
+                if (numbers[i] == numbers[i + 1])
+                {
+                    numbers[i + 1] = numbers[i] + numbers[i + 1];
+                    numbers.RemoveAt(i);
+                    i = -1;
+                }
+            }
+            numbers.RemoveAt(numbers.Count - 1);
+            Console.WriteLine(string.Join(" ", numbers));
+        }
+    }
+}
