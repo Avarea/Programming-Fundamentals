87a376195cbc6763fc4060fcdfadc1e9cb809be5
Avarea <valchek@abv.bg>
2017-06-20 15:38:34 +0300

    Lists - 1. Remove Negatives and Reverse

---
+++ Lists/01RemoveNegativesAndReverse/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01RemoveNegativesAndReverse
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = Console.ReadLine().Split(' ').Select(int.Parse).ToList();
+            List<int> newList = new List<int>();
+
+            for (int cnt = 0; cnt < numbers.Count; cnt++)
+            {
+                if (numbers[cnt] > 0)
+                {
+                    newList.Add(numbers[cnt]);
+                }
+            }
+            newList.Reverse();
+            if (newList.Count == 0)
+            {
+                Console.WriteLine("empty");
+            }
+            else
+            {
+                Console.WriteLine(string.Join(" ", newList));
+
+            }
+        }
+    }
+}
