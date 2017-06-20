0d528856aca35936314318df29fbf73ab0f9e465
Avarea <valchek@abv.bg>
2017-06-20 23:48:42 +0300

    C# CONDITIONAL STATEMENTS AND LOOPS - EXERCISES

---
+++ AllProblems-Updated/ChooseADrink/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace ChooseADrink
+{
+    class Program
+    {
+        static void Main()
+        {
+            string profession = Console.ReadLine();
+            Console.WriteLine(GetADrink(profession));
+        }
+
+        private static string GetADrink(string profession)
+        {
+            string result = string.Empty;
+            if (profession == "Athlete")
+            {
+                result = "Water";
+            }
+            else if ((profession == "Businessman") || (profession == "Businesswoman"))
+            {
+                result = "Coffee";
+            }
+            else if (profession == "SoftUni Student")
+            {
+                result = "Beer";
+            }
+            else
+            {
+                result = "Tea";
+            }
+            return result;
+        }
+    }
+}
---
+++ AllProblems-Updated/ChooseADrink2.0/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace ChooseADrink2._0
+{
+    class Program
+    {
+        static void Main()
+        {
+            string profession = Console.ReadLine();
+            int quantity = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("The {0} has to pay {1:f2}", profession, quantity * GetADrink(profession));
+        }
+
+        private static double GetADrink(string profession)
+        {
+            double result;
+            if (profession == "Athlete")
+            {
+                result = 0.7;
+            }
+            else if ((profession == "Businessman") || (profession == "Businesswoman"))
+            {
+                result = 1.00;
+            }
+            else if (profession == "SoftUni Student")
+            {
+                result = 1.7;
+            }
+            else
+            {
+                result = 1.2;
+            }
+            return result;
+        }
+    }
+}
