991f1820d62ebda098b73eb3e59c777fa433b225
Avarea <valchek@abv.bg>
2017-06-20 19:38:06 +0300

    Lists - 7.Count Numbers using Dictionary

---
+++ Lists/07CountNumbersWithDictionary/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+
+namespace _07CountNumbersWithDictionary
+{
+    class Program
+    {
+        static void Main()
+        {
+            var input = Console.ReadLine().Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+            Dictionary<int, int> dict = new Dictionary<int, int>();
+            foreach (var num in input)
+            {
+                if (dict.ContainsKey(num))
+                {
+                    dict[num] += 1;
+                }
+                else
+                {
+                    dict.Add(num, 1);
+                }
+            }
+            foreach (var d in dict.OrderBy(n => n.Key))
+            {
+                Console.WriteLine($"{d.Key} -> {d.Value}");
+            }
+        }
+    }
+}
