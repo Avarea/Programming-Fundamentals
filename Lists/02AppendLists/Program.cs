74e8f51389980c30ab473d48df7ea6c352fd1ab0
Avarea <valchek@abv.bg>
2017-06-20 16:03:32 +0300

    Lists - 2. Append Lists

---
+++ Lists/02AppendLists/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+
+namespace _02AppendLists
+{
+    class Program
+    {
+        static void Main()
+        {
+            List<string> numbers = Console.ReadLine().Split(new char[] { '|' }).ToList();
+            string result = string.Empty;
+
+            for (int cnt = numbers.Count - 1; cnt >= 0 ; cnt--)
+            {
+                string element = numbers[cnt];
+                var differentPats = element.Split(new char[] { ' ' },StringSplitOptions.RemoveEmptyEntries).ToList();
+
+                for (int i = 0; i < differentPats.Count; i++)
+                {
+                    result += differentPats[i] + " ";
+                }
+            }
+            Console.WriteLine(result);
+        }
+    }
+}
