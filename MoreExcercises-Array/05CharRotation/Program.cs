1f3dee5fe8698eca866ea5360aafaaec2e3a8d1c
Avarea <valchek@abv.bg>
2017-06-20 14:20:48 +0300

    More Exercises: Arrays - 5 - Char Rotation

---
+++ MoreExcercises-Array/05CharRotation/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Linq;
+
+
+namespace _05CharRotation
+{
+    class Program
+    {
+        static void Main()
+        {
+            string characters = Console.ReadLine();
+            int[] numbers = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+            char[] result = new char[numbers.Length];
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                if (numbers[i] % 2 == 0)
+                {
+                    result[i] += (char)(characters[i] - numbers[i]);
+                }
+                else
+                {
+                    result[i] += (char)(characters[i] + numbers[i]);
+                }
+            }
+            Console.WriteLine(result);
+        }
+    }
+}
