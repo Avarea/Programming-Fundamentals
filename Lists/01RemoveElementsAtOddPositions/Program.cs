f3ed7c0e757d8528ff554a91991138a98dd19054
Avarea <valchek@abv.bg>
2017-06-27 12:52:58 +0300

    Lists - 1. Remove Elements at Odd Positions

---
+++ Lists/01RemoveElementsAtOddPositions/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01RemoveElementsAtOddPositions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> list = Console.ReadLine().Split(' ').ToList();
+            List<string> result = new List<string>();
+            for (int i = 0; i < list.Count; i++)
+            {
+                if (i % 2 == 1)
+                {
+                    result.Add(list[i]);
+                }
+            }
+            Console.WriteLine(String.Join("", result));
+        }
+    }
+}
